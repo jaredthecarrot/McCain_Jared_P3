69984f0e89bfd8ee8c5301f5a19b3baf8b7c1226
Jared McCain <jaredmccain.jam@gmail.com>
2023-02-22 12:45:07 -0600

    rough draft 1

--- McCain_Jared_P3.cpp
+++ McCain_Jared_P3.cpp
@@ -1,1 +1,6 @@
 #include "McCain_Jared_P3.h"
+
+int main(int c, char* argv[])
+{
+    return 0;
+}
--- McCain_Jared_P3.h
+++ McCain_Jared_P3.h
@@ -2,23 +2,78 @@
 #define McCain_Jared_P3.h
 
 template <typename E>
+
 class Node {
     public:
-E data;
-Node* next;
-Node* prev;
+    
+    E data;
+
+    Node* next;
+
+    Node* prev;
 };
 
 template<typename E>
 class PriorityQueue{
     private:
+
     Node* head;
+
     Node* tail;
+
+    int countS;
+
     public:
+
+    PriorityQueue();
+
     void insert(E& e);
+
     void removeMin();
+
     void min();
-    const int size();
-    const bool empty();
+
+    int size() const;
+
+    bool empty() const;
+
 };
+
+template<typename E>
+
+PriorityQueue<E>::PriorityQueue(){
+    head = nullptr;
+
+    tail = nullptr;
+}
+
+template<typename E>
+
+void PriorityQueue<E>::insert(E& e){
+//INCOMPLETE NEEDS TO INSERT INTO FILE
+}
+
+template<typename E>
+
+void PriorityQueue<E>::removeMin(){
+//INCOMPLETE MAY NOT NEED
+}
+
+template<typename E>
+
+void PriorityQueue<E>::min(){
+//INCOMPLETE
+}
+
+template<typename E>
+
+int PriorityQueue<E>::size() const {
+    return countS;
+}
+
+template<typename E>
+
+bool PriorityQueue<E>::empty() const {
+    return (head == nullptr);
+}
 #endif
